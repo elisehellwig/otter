50875623afa57aa72d8baaa490dcd3dd68277a6f
elisehellwig <echellwig@ucdavis.edu>
2017-07-08 22:36:51 -0500

    just looking at some things! nothing is popping out right away though

--- explore.R
+++ explore.R
@@ -1,12 +1,18 @@
-datapath <- '/Users/echellwig/Drive/OtherPeople/otter'
+datapath <- '/Users/echellwig/Drive/OtherPeople/otterData'
 options(stringsAsFactors = FALSE)
 
 otr <- read.csv(file.path(datapath, 'otterclean.csv'))
 
 ################################################################
 
+countindex <- which(count(otr$Area)$freq==5)
 
+allyrnames <- count(otr$Area)[countindex,'x']
 
+allyears <- otr[otr$Area %in% allyrnames, ]
 
+totals <- sapply(unique(otr$Year), function(y) {
+    sum(allyears[allyears$Year==y, 'Pop'])
+})
 
-
+tdf <- data.frame(year=unique(otr$Year), pop=totals)
