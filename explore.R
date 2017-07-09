b326494c44d199a86c9201c3df2550787e29e25b
elisehellwig <echellwig@ucdavis.edu>
2017-07-08 22:17:47 -0500

    Just setting up for the expolratory analysis

---
+++ explore.R
@@ -0,0 +1,12 @@
+datapath <- '/Users/echellwig/Drive/OtherPeople/otter'
+options(stringsAsFactors = FALSE)
+
+otr <- read.csv(file.path(datapath, 'otterclean.csv'))
+
+################################################################
+
+
+
+
+
+
