2d11ce66e9a5fafceb6cae750839c4c235da8801
elisehellwig <echellwig@ucdavis.edu>
2017-07-08 22:10:14 -0500

    just wrote the script that imports the data and cleans it.

---
+++ import.R
@@ -0,0 +1,27 @@
+datapath <- '/Users/echellwig/Drive/OtherPeople/otter'
+library(reshape2)
+library(plyr)
+library(rethinking)
+
+options(stringsAsFactors = FALSE)
+
+ott <- read.csv(file.path(datapath, 'otterpopAll.csv'))
+
+areaNames <- c('Abbots', "NTB", "Giacomini", 'Rodeo', "Muir",'Madera',
+               'Tennessee', "Bolinas",'Drakes','Bass','Alpine','Peters',
+               'LasGallinas','Estero')
+sector <- c('North','North','North','South','South','Bay','South','South',
+            'North', 'Coast', 'Lagunitas','Lagunitas', 'Bay','North') 
+
+#####################################################
+
+ott$Area <- areaNames
+ott$sector <- sector
+ottm <- melt(ott, id.vars = c('Area','sector'), variable.name = 'Year', value.name = 'Pop')
+
+ottm$Year <- as.integer(sub("X", "", ottm$Year))
+
+NApops <- which(is.na(ottm$Pop))
+ottr <- ottm[-NApops,]
+
+write.csv(ottr, file.path(datapath, 'otterclean.csv'), row.names = FALSE)
