0f31b00b36cb9144e24fea19995191239cf6fd72
elisehellwig <echellwig@ucdavis.edu>
2017-07-08 22:36:25 -0500

    Added code to repeat everything for the pup data.

--- import.R
+++ import.R
@@ -1,9 +1,11 @@
-datapath <- '/Users/echellwig/Drive/OtherPeople/otter'
+datapath <- '/Users/echellwig/Drive/OtherPeople/otterData'
 library(reshape2)
 
 options(stringsAsFactors = FALSE)
 
 ott <- read.csv(file.path(datapath, 'otterpopAll.csv'))
+pup <- read.csv(file.path(datapath, 'otterpopPups.csv'))
+
 
 areaNames <- c('Abbots', "NTB", "Giacomini", 'Rodeo', "Muir",'Madera',
                'Tennessee', "Bolinas",'Drakes','Bass','Alpine','Peters',
@@ -23,3 +25,19 @@ NApops <- which(is.na(ottm$Pop))
 ottr <- ottm[-NApops,]
 
 write.csv(ottr, file.path(datapath, 'otterclean.csv'), row.names = FALSE)
+
+
+
+pup$Area <- areaNames
+pup$sector <- sector
+pupm <- melt(pup, id.vars = c('Area','sector'), variable.name = 'Year', value.name = 'Pop')
+
+pupm$Year <- as.integer(sub("X", "", pupm$Year))
+
+NApops <- which(is.na(pupm$Pop))
+pupr <- pupm[-NApops,]
+
+write.csv(pupr, file.path(datapath, 'pupclean.csv'), row.names = FALSE)
+
+
+
