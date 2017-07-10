62d1169c3ec1d7000eb14afca7843f00f00cbfff
elisehellwig <echellwig@ucdavis.edu>
2017-07-10 17:42:27 -0500

    ran a poisson model (not multilevel, I don't think it really is what we want.)

--- 3_glm.R
+++ 3_glm.R
@@ -1,4 +1,21 @@
 datapath <- '/Users/echellwig/Drive/OtherPeople/otterData'
 options(stringsAsFactors = FALSE)
+library(rethinking)
+rstan_options(auto_write = TRUE)
 
 otr <- read.csv(file.path(datapath, 'otterclean.csv'))
+
+#########################################################
+
+mod1 <- map(
+    alist(pop ~ dpois(lambda),
+          log(lambda) <- a + by*year,
+          a ~ dnorm(0, 100),
+          by ~ dnorm(0, 1),
+          bl ~ dnorm(0, 1)
+    ), data=otr)
+
+
+
+
+mod2 <- map2stan()
