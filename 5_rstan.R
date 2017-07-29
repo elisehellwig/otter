a68151a4c8d9fb6e77b054eabfcc3d21bd2ea0df
elisehellwig <echellwig@ucdavis.edu>
2017-07-29 12:15:27 -0500

    adding code to evaluate spliting data completely (no pooling) and also the multilevel models (partial pooling)

--- 5_rstan.R
+++ 5_rstan.R
@@ -4,67 +4,77 @@ options(mc.cores = parallel::detectCores())
 library(rstan)
 
 otr <- read.csv(file.path(datapath, 'otterclean.csv'))
+linear <- readRDS(file.path(datapath, 'models/linear.RDS'))
+linearsplit <- readRDS(file.path(datapath, 'models/linearsplit.RDS'))
+vl <- readRDS(file.path(datapath, 'models/varyinglocation.RDS'))
 
+source('functions.R')
 
+otr$cpop <- scale(otr$pop) #4.62 center; 2.15 scale
+attributes(otr$cpop) <- NULL
 ##########################################################
+
+locfac <- factor(otr$location)
+
+binaryvars <- model.matrix(~location, data=otr)[,-1]
+
+bvlist <- lapply(1:12, function(i) {
+    unname(binaryvars[,i])
+})
+names(bvlist) <- levels(locfac)[-1]
+
 
 #data for lm stan model
 fixedlist <- list(pop=otr$pop,
                   year=otr$year,
                   N=nrow(otr))
 
+splitlist <- c(fixedlist, bvlist)
 
-#just a normal linear model
-otrfixed <- '
-data{
-    int<lower=1> N; //number of data points
-    real<lower=0> pop[N]; //dependent variable, otter population
-    real year[N]; //predictor variable, year
-}
-parameters{
-    vector[2] beta;
-    real <lower=0> sigma;
-}  
-model {
-    real mu;
-    for (i in 1:N) {
-        mu = beta[1] + beta[2] * year[i];
-        pop[i] ~ normal(mu, sigma);
-    }
-}
-'
+multilist <- list(pop=otr$pop,
+                  year=otr$year,
+                  N=nrow(otr),
+                  P=nlevels(locfac),
+                  loc=as.integer(locfac))
 
-fixedpost <- stan(model_code=otrfixed, data=fixedlist, iter=2000, chains=4)
 
-#multi-level model
-otrlmm <- '
-data{
-    int<lower=1> N; //number of data points
-    real<lower=0> pop[N]; //dependent variable, otter population
-    real year[N]; //predictor variable, year
-    int P; //number of locations
-    int loc[P] <lower=1, upper=L>; //location id
-}
-parameters{
-    vector[2] beta;
-    real <lower=0> sigma;
-    vector<lower=0>[2] sigma_U;
-    cholesky_factor_corr[2] L_p;
-    matrix[2,P] z_p;
-}  
-transformed parameters {
-    matrix[2,P] p;
-    p <- diag_pre_multiply(sigma_p, L_p) * z_p;  //loc random effects
-}
-model {
-    real mu;
-    //priors
-    L_p ~ lkj_corr_cholesky(2.0);
-    to_vector(z_p) ~ normal(0,1);
-    //likelihood
-    for (i in 1:N) {
-        mu = beta[1] + p[1,loc[i]] + (beta[2] + i[2, loc[i]]) * year[i];
-        pop[i] ~ normal(mu, sigma);
-    }
-}
-'
+
+##################################
+#otter population models
+
+
+#fixed FX model
+fixedl <- stan(model_code=linear, data=fixedlist, iter=20000, warmup = 5000,
+               chains=1)
+saveRDS(multilong, file.path(datapath, 'models/fixedpost.RDS'))
+
+
+#fixed split FX model
+fixedsplit <- stan(model_code=linearsplit, data=splitlist, iter=4000, 
+                   warmup = 2000, chains=4)
+
+
+
+#Varying FX location model
+
+#identifying any pathologies in the model
+#multi <- stan(model_code=vl, data=multilist, iter=8000, chains=4, 
+             # control=list(adapt_delta = 0.99))
+
+#running the model for estimating parameters etc.
+multilong <- stan(model_code=vl, data=multilist, iter=20000, warmup=5000,
+                  chains=1, control=list(adapt_delta = 0.99))
+saveRDS(multilong, file.path(datapath, 'models/varyinglocationpost.RDS'))
+
+multipost <- locpost(multilong, 6)
+
+multidecline <- sapply(1:13, function(i) declinerisk(multilong, i, locpost)) 
+multibeta2 <- sapply(1:13, function(i) mean(locpost(multilong, i)$b1))
+multidf <- data.frame(location=levels(locfac),
+                      b1=round(multibeta2, 3),
+                      declineprob=round(multidecline, 3))
+
+
+
+library(rethinking)
+coeftab(multilong)
