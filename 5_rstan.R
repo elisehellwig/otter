15a750fcae6c436aff11edb977ac79c2157511fc
elisehellwig <echellwig@ucdavis.edu>
2017-08-01 11:42:42 -0500

    running all of the stan models (no analysis), including pups

--- 5_rstan.R
+++ 5_rstan.R
@@ -5,7 +5,8 @@ library(rstan)
 
 otr <- read.csv(file.path(datapath, 'otterclean.csv'))
 linear <- readRDS(file.path(datapath, 'models/linear.RDS'))
-vl <- readRDS(file.path(datapath, 'models/varyinglocation.RDS'))
+vl1 <- readRDS(file.path(datapath, 'models/varying1location.RDS'))
+vl2 <- readRDS(file.path(datapath, 'models/varying2location.RDS'))
 
 source('functions.R')
 
@@ -40,12 +41,19 @@ saveRDS(fixedl, file.path(datapath, 'models/fixedpost.RDS'))
 #Varying FX location model
 
 #identifying any pathologies in the model
-#multi <- stan(model_code=vl, data=multilist, iter=8000, chains=4, 
-             # control=list(adapt_delta = 0.99))
+multi1 <- stan(model_code=vl1, data=multilist, iter=25000, warmup=5000,
+               chains=1, control=list(adapt_delta = 0.99))
+
+saveRDS(multi1, file.path(datapath, 'models/varying1locationpost.RDS'))
+
 
 #running the model for estimating parameters etc.
-multilong <- stan(model_code=vl, data=multilist, iter=20000, warmup=5000,
+multi2long <- stan(model_code=vl, data=multilist, iter=20000, warmup=5000,
                   chains=1, control=list(adapt_delta = 0.99))
-saveRDS(multilong, file.path(datapath, 'models/varyinglocationpost.RDS'))
+saveRDS(multilong, file.path(datapath, 'models/varying2locationpost.RDS'))
 
+
+###########################################
+
+multi1pup <- 
 
