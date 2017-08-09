7c0f744f8935457792f68166f8e4cc805f6e8e52
elisehellwig <echellwig@ucdavis.edu>
2017-08-08 20:37:02 -0500

    have updated and run all the pup models

--- 6_rstan_pup.R
+++ 6_rstan_pup.R
@@ -33,23 +33,27 @@ multilist <- list(pups=pupr$pups,
 
 
 #fixed FX model
-fixedl <- stan(model_code=linear, data=fixedlist, iter=4000, warmup = 2000,
-               chains=4)
+fixedl <- stan(model_code=linear, data=fixedlist, iter=20000, warmup = 5000,
+               chains=1)
 saveRDS(fixedl, file.path(datapath, 'models/pupfixedpost.RDS'))
 
 #Varying FX location model
 
 #identifying any pathologies in the model
-multi1 <- stan(model_code=vl1, data=multilist, iter=25000, warmup=5000,
+multi1 <- stan(model_code=pvl1, data=multilist, iter=20000, warmup=5000,
                chains=1, control=list(adapt_delta = 0.99))
 
-saveRDS(multi1, file.path(datapath, 'models/varying1locationpost.RDS'))
+saveRDS(multi1, file.path(datapath, 'models/pupvarying1locationpost.RDS'))
 
 
 #running the model for estimating parameters etc.
-multi2long <- stan(model_code=vl, data=multilist, iter=20000, warmup=5000,
-                  chains=1, control=list(adapt_delta = 0.99))
-saveRDS(multilong, file.path(datapath, 'models/varying2locationpost.RDS'))
+multi2 <- stan(model_code=pvl2, data=multilist, iter=20000, warmup=5000,
+                  chains=4, control=list(adapt_delta = 0.99))
+saveRDS(multi2, file.path(datapath, 'models/pupvarying2locationpost.RDS'))
 
 
-###########################################################################
+
+
+
+
+
