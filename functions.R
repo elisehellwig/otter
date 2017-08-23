439a0bcd2437c4eb00b87ad0b24d09d5ad948a13
elisehellwig <echellwig@ucdavis.edu>
2017-08-23 13:52:10 -0500

    wrote a function to extract variable numbers of sampled parameters that I can use in my locpost function

--- functions.R
+++ functions.R
@@ -1,9 +1,91 @@
-locpost <- function(sfit, location) {
-    #sfit is a fitted stan model
-    #location is a number from 1-13
-    varyingpars <- paste0('z_u[', 1:2, ',',location,']')
+extractpar <- function(stanmodel, parameter, column, stat=NA, 
+                       columnnames=NA, location=NA, rows=c(1,2)) {
     
-    fixedpars <- c('beta[1]','beta[2]')
+    require(rstan)
+    pd <- sfit@par_dims[[parameter]]
+    
+    if (length(pd)==0) {
+        parvec <- parameter
+    } else if (length(pd)==1) {
+        
+        if (is.na(location)) {
+            parvec <- paste0(parameter, '[', 1:pd, ']') 
+        } else {
+            parvec <- paste0(parameter, '[', location, ']') 
+        }
+        
+    } else if (length(pd)==2) {
+        
+        if (length(setdiff(rows, 1:pd[1]) > 0)) {
+            stop('You can only extract as many varying parameters as there are in the model')
+        }
+        
+        if (is.na(location)) {
+            irows <- rep(rows, each=pd[2])
+            icols <- rep(1:pd[2], length(rows))
+            parvec <- paste0(parameter, '[', irows, ',', icols, ']')
+        } else {
+            
+            parvec <- paste0(parameter, '[', rows, ',', location, ']')
+        }
+        
+    } else {
+        stop('parameter dimension cannot be negative.')
+    }
+        
+    
+    ps <- extract(stanmodel, par=parvec)
+    
+    return(ps)
+}
+
+
+extractStanPars <- function(stanmodel, parameter, column, stat=NA, 
+                            columnnames=NA, location=NA) {
+    require(rstan)
+    
+   
+
+    
+    
+    return(parestimates)
+}
+
+postxy <- function(samples) {
+    
+}
+
+
+
+
+locpost <- function(sfit, location=NA, cholesky=FALSE, par_name='beta', 
+                    blockid=NA) {
+    #sfit is a fitted stan model
+    #location is a number from 1-14
+    
+    pd <- sfit@par_dims
+    
+    if (is.na(blockid)) {
+        beta0samples <- extract(sfit, pars=c(fixedpars[1], varyingpars[1]))
+            
+    } else {
+        if (is.na(location)) {
+            poi <- c(par_name, blockid)
+    
+        } else {
+            
+            if (pd[[blockid]][1]==1) {
+                poi <- c('beta', paste0(blockid,'[', location, ']'))
+                
+            } else if (pd[[blockid]][1]==2) {
+                poi <- c('beta', paste0(blockid,'[', 1:2, ',' , location, ']'))
+            } else {
+                stop('There can only be 1 or 2 varying parameters at this time')
+            }
+            
+        }
+            
+    }
     
     beta0samples <- extract(sfit, pars=c(fixedpars[1], varyingpars[1]))
     beta1samples <- extract(sfit, pars=c(fixedpars[2], varyingpars[2]))
@@ -28,35 +110,5 @@ declinerisk <- function(sfit, location, postfun) {
     return(dprob)
     
 }
-
-
-extractStanPars <- function(stanmodel, parameter, column, stat=NA, 
-                            columnnames=NA) {
-    require(rstan)
-    
-    samples <- as.data.frame(extract(stanmodel, pars=parameter))
-    searchpattern <- paste0(parameter, '.', column, '.')
-    cols <- grep(searchpattern, names(samples))
-    
-    selectedsamples <- samples[,cols]
-    
-    if (!is.na(columnnames[1])) {
-        names(selectedsamples) <- columnnames
-    }
-    
-    
-    if (is.na(stat)) {
-        parestimates <- selectedsamples
-    } else {
-        parestimates <- data.frame(location=columnnames,
-                                   estimate=apply(selectedsamples, 2, stat))
-    }
-   
-    
-    return(parestimates)
-}
-
-
-
 
 
