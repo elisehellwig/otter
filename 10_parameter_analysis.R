7abb3547ee602cd9eca961a81381f07999919d02
elisehellwig <echellwig@ucdavis.edu>
2017-10-04 11:31:50 -0700

    a script to see if we can figure out a way to explain why some otters are doing well and some are not.

---
+++ 10_parameter_analysis.R
@@ -0,0 +1,59 @@
+datapath <- '/Users/echellwig/Drive/OtherPeople/otterData'
+options(stringsAsFactors = TRUE)
+library(rstan)
+library(ggplot2)
+library(rethinking)
+library(randomForest)
+library(glmnet)
+
+
+av <- read.csv(file.path(datapath, 'allvars.csv'))
+
+
+
+####################################################################
+####################################################################
+
+firstp <- 10
+
+### Random forest
+declinePformula <- createformula(av, 'declineP', firstp)
+declinerf <- randomForest(declinePformula, data=av)
+
+
+betaformula <- createformula(av, 'beta', firstp)
+betarf <- randomForest(betaformula, data=av)
+
+
+alphaformula <- createformula(av, 'alpha', firstp)
+alpharf <- randomForest(alphaformula, data=av)
+
+
+metricformula <- createformula(av, 'metric', firstp)
+metricrf <- randomForest(metricformula, data=av)
+
+
+
+##############################################################
+#shrinkage methods
+shrinkcols <- c('declineP', 'beta','alpha', 'SFOdistance','SFOtime',
+                'PopDensity','logDensity','Longitude','Latitude')
+avs <- av[,shrinkcols]
+binaryregion <- convertbinary(av$region)
+binaryhabitat <- convertbinary(av$habitat)
+binaryaccess <- convertbinary((av$Access))
+avs <- cbind(avs, binaryregion, binaryhabitat, binaryaccess)
+
+
+declinelass <- glmnet(x=as.matrix(avs[,4:ncol(avs)]), y=avs$declineP, 
+                      family='gaussian', alpha=1)
+
+
+sanitycheck <- lm(beta ~ habitat, data=av)
+
+
+#forward/backward selection
+
+
+
+
