21c3a3ce1a69ca4d9b9f6a620419050fe3a9bd3f
elisehellwig <echellwig@ucdavis.edu>
2017-12-20 18:13:35 -0500

    computed moran's I for all the variables we are interested in. Still need to do the residuals

--- 9_autocorr.R
+++ 9_autocorr.R
@@ -1,6 +1,7 @@
 datapath <- '/Users/echellwig/Drive/OtherPeople/otterData'
 library(spdep)
 library(raster)
+library(rgdal)
 
 
 av <- read.csv(file.path(datapath, 'allvars.csv'))
@@ -13,17 +14,31 @@ av <- read.csv(file.path(datapath, 'allvars.csv'))
 avsp <- SpatialPointsDataFrame(coords=av[,c('Longitude','Latitude'), ],
                                proj4string=CRS("+init=epsg:4326"), data=av)
 
+writeOGR(avsp, dsn=file.path(datapath, 'otterSPDF.GeoJSON'),
+         layer='"OGRGeoJSON"', driver='GeoJSON')
+
 knear <- knearneigh(avsp, k=3, longlat=TRUE)
 knb <- knn2nb(knear, row.names = avsp$loc)
+saveRDS(knb, file.path(datapath, 'kNearOtters.RDS'))
 
 distnb <- dnearneigh(avsp, d1=0, d2=10, longlat=TRUE, row.names=avsp$loc)
+saveRDS(distnb, file.path(datapath, 'DistanceOtters.RDS'))
 
 
 klist <- nb2listw(knb)
 distlist <- nb2listw(distnb)
-kmoran <- moran.mc(avsp$metric, klist, 9999)
-distmoran <- moran.mc(avsp$metric, distlist, 9999)
 
+kmetric <- moran.mc(avsp$metric, klist, 99999)
+distmetric <- moran.mc(avsp$metric, distlist, 99999)
 
-                               
+kalpha <- moran.mc(avsp$alpha, klist, 99999)
+distalpha <- moran.mc(avsp$alpha, distlist, 99999)
 
+kbeta <- moran.mc(avsp$beta, klist, 99999)
+distbeta <- moran.mc(avsp$beta, distlist, 99999)
+
+kDP <- moran.mc(avsp$declineP, klist, 99999)
+distDP <- moran.mc(avsp$declineP, distlist, 99999)
+
+kAres <- moran.mc(avsp$Ares, klist, 99999)
+distAres <- moran.mc(avsp$Ares, distlist, 99999)
